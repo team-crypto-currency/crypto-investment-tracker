79f97552e43f44beb9270dd9342118d9c3853742
Pro-Code-art <jayme.coder@gmail.com>
2021-02-23 21:22:54 -0800

    set up server and handlebars to render a bootstrap navbar/ all static files are linked and working properly/ successful ajax call to coin ranking api

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-
+// Middleware to parse req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve our public folder/static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up Handlebars as template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-
-
-
-
+// Require/import routes
+require('./routes/api-routes.js')(app);
+// require('./routes/html-routes.js')(app);
 
 
 // Set server to listen for requests/ ? Do we need db.sync for sequelize for this to create tables?
+app.listen(PORT, () => {
+    console.log(`App listening on URL: http://localhost:${PORT}`);
+  });
