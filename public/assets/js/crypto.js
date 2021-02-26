9e908549269108073ae5e260ca48cc7c81224f6a
haywah27 <hayleywahlroos15@gmail.com>
2021-02-25 22:13:17 -0800

    Added initial  MACD call through the backend api-route

--- public/assets/js/crypto.js
+++ public/assets/js/crypto.js
@@ -3,123 +3,108 @@
 
 
 // Successful api call for coins/prices! with coin-ranking api (See console in browser)
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://coinranking1.p.rapidapi.com/coins",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
-            "x-rapidapi-host": "coinranking1.p.rapidapi.com"
-        }
-    };
-    $.ajax(settings).done(function (response) {
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://coinranking1.p.rapidapi.com/coins/",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
+        "x-rapidapi-host": "coinranking1.p.rapidapi.com"
+    }
+};
+$.ajax(settings).done(function (response) {
 
-        console.log("ajax1:", response);
-    });
+    console.log("ajax1:", response);
+});
 
 
-    const settings2 = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://coinranking1.p.rapidapi.com/coins",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
-            "x-rapidapi-host": "coinranking1.p.rapidapi.com"
-        }
-    };
+// const settings2 = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": `https://coinranking1.p.rapidapi.com/coin/${coinID}`,
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
+//         "x-rapidapi-host": "coinranking1.p.rapidapi.com"
+//     }
+// };
+// $.ajax(settings2).done(function (response) {
 
-    // const settings3 = {
-    //     "async": true,
-    //     "crossDomain": true,
-    //     "url": `https://coinranking1.p.rapidapi.com/coin/${coinID}`,
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
-    //         "x-rapidapi-host": "coinranking1.p.rapidapi.com"
-    //     }
-    // };
-    // $.ajax(settings2).done(function (response) {
+//     console.log("ajax2:", response);
+// });
 
-    //     console.log("ajax2:", response);
-    // });
-    
 
-    const searchBtn = $(".searchBtn");
-    const searchBox = $(".searchFld");
-    const coinSearch = $(".currentSrch");
-    const coinDetails = $(".coinDetail");
-    const currentDate = (moment().format("M/D/YY"));
+const searchBtn = $(".searchBtn");
+const searchBox = $(".searchFld");
+const coinSearch = $(".currentSrch");
+const coinDetails = $(".coinDetail");
+const currentDate = (moment().format("M/D/YY"));
 
-    searchBtn.click(function(){
-        const searchedCoin = $(searchBox).val();
-        renderManyCoins(searchedCoin)
-    });
-    
-    
-    let thisCoin;
-    function renderManyCoins(searchedCoin) { 
-        coinDetails.empty();
-        $.ajax(settings2).then(function(response) {
+searchBtn.click(function(){
+    const searchedCoin = $(searchBox).val();
+    renderManyCoins(searchedCoin)
+});
 
-            for(let i=0;i<response.data.coins.length;i++) {
-                // console.log(coin)
-                if (searchedCoin === response.data.coins[i].name || searchedCoin === response.data.coins[i].symbol){
-                    console.log(response.data.coins[i].name)
-                    thisCoin = response.data.coins[i];
-                    console.log(thisCoin)
-                }
-            };
 
-            
-            const coinName = thisCoin.name;
+let thisCoin;
+function renderManyCoins(searchedCoin) { 
+    coinDetails.empty();
+    $.ajax(settings).then(function(response) {
 
-            const coinPriceToInt = parseFloat(thisCoin.price).toFixed(2);
+        for(let i=0;i<response.data.coins.length;i++) {
+            // console.log(coin)
+            if (searchedCoin === response.data.coins[i].name){
+                console.log("user search name match: ", response.data.coins[i].name)
+                thisCoin = response.data.coins[i];
+                console.log("this coin: ", thisCoin)
+            }
+        };
 
-            const changeVar = thisCoin.change;
-
-            const highestVar = thisCoin.allTimeHigh.timestamp;
-
-            const highPriceToInt = parseFloat(thisCoin.allTimeHigh.price).toFixed(2);
-            const currentPrice = $("<p>").text(`Current Price: $${coinPriceToInt}`);
-
-            const change = $("<p>").text(`(Real Time) Change: ${changeVar} %`);
-
-            const highMarkTime = `Hit on: ${moment(highestVar).format("MM/DD/YYYY")}`
-
-            const highMark = $("<p>").text(`All Time High: $${highPriceToInt} ${highMarkTime}`);
-
-            // const oneMonth = $("<p>").text(`Price One Month Ago: $${monthPrice}`);
-
-            
-
-            
-            $(coinSearch).text(`${coinName} (${currentDate})`);
-            $(coinDetails).append(currentPrice, change, highMark);
         
+        const coinName = thisCoin.name;
 
-        // const monthPrice = parseFloat(response.data.coins[0].allTimeHigh.price).toFixed(2);
+        const coinABRV = thisCoin.symbol;
 
-        // const yearPrice;
+        const coinPriceToInt = parseFloat(thisCoin.price).toFixed(2);
 
-      });
-    
-    }
+        const changeVar = thisCoin.change;
+
+        const highestVar = thisCoin.allTimeHigh.timestamp;
+
+        const highPriceToInt = parseFloat(thisCoin.allTimeHigh.price).toFixed(2);
+        const currentPrice = $("<p>").text(`Current Price: $${coinPriceToInt}`);
+
+        const change = $("<p>").text(`Change: ${changeVar}`);
+
+        const highMarkTime = `Hit on: ${moment(highestVar).format("MM/DD/YYYY")}`
+
+        const highMark = $("<p>").text(`All Time High: $${highPriceToInt} ${highMarkTime}`);
 
 
+        $(coinSearch).text(`${coinName} (${currentDate})`);
+        $(coinDetails).append(currentPrice, change, highMark);
 
-    
-    function renderSingleCoin(coinName, coinID) {
+        renderMACD(coinABRV);
 
-        if (searchedCoin === coinName) {
-            
-        }
-        $.ajax(settings2).then(function(response) {
+  });
 
-        });
-    }
+}
 
-    // search coin
-    // find in ajax call 1
-    // run ajax call 2 with id from ajax 1
+function renderMACD(coin){
+    console.log("abrev: ", coin);
+
+    const settings3 = {
+        "async": true,
+        "crossDomain": true,
+        "url": `/api/coin/${coin}`,
+        "method": "GET",
+    };
+    $.ajax(settings3).done(function (response) {
+
+        console.log("MACD:", response);
+        
+    });
+
+}
+
