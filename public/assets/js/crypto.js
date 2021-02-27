654a74375229ca93d8b0de7f10a272f79e97b10f
Pro-Code-art <jayme.coder@gmail.com>
2021-02-27 15:25:32 -0800

    wrote api route to get users and their coins by id, and wrote function for sign in button in crypto.js that includes fetch call to get user's coins

--- public/assets/js/crypto.js
+++ public/assets/js/crypto.js
@@ -1,21 +1,17 @@
-// Create an ajax call with jQuery to test apis 
-    // Taapi.io for indicators
-
 
 // Successful api call for coins/prices! with coin-ranking api (See console in browser)
 const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://coinranking1.p.rapidapi.com/coins/",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
-        "x-rapidapi-host": "coinranking1.p.rapidapi.com"
-    }
+  "async": true,
+  "crossDomain": true,
+  "url": "https://coinranking1.p.rapidapi.com/coins/",
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "198c5d9404msh8afbdbe95aa7f12p115299jsn1d3f7e17a3ea",
+    "x-rapidapi-host": "coinranking1.p.rapidapi.com"
+  }
 };
 $.ajax(settings).done(function (response) {
-
-    console.log("ajax1:", response);
+  console.log("ajax1:", response);
 });
 
 
@@ -43,101 +39,114 @@ const currentDate = (moment().format("M/D/YY"));
 const saveBtn = $("<button>").text("Save Coin").addClass("save-btn");
 
 searchBtn.click(function(){
-    const searchedCoin = $(searchBox).val();
-    renderManyCoins(searchedCoin)
+  const searchedCoin = $(searchBox).val();
+  renderManyCoins(searchedCoin);
 });
 
 
 let thisCoin;
-function renderManyCoins(searchedCoin) { 
-    coinDetails.empty();
-    $.ajax(settings).then(function(response) {
+function renderManyCoins(searchedCoin) {
+  coinDetails.empty();
+  $.ajax(settings).then(function(response) {
 
-        for(let i=0;i<response.data.coins.length;i++) {
-            // console.log(coin)
-            if (searchedCoin === response.data.coins[i].name){
-                console.log("user search name match: ", response.data.coins[i].name)
-                thisCoin = response.data.coins[i];
-                console.log("this coin: ", thisCoin)
-            }
-        };
+    for(let i=0;i<response.data.coins.length;i++) {
+    // console.log(coin)
+      if (searchedCoin === response.data.coins[i].name) {
+        console.log("user search name match: ", response.data.coins[i].name);
+        thisCoin = response.data.coins[i];
+        console.log("this coin: ", thisCoin);
+      }
+    }
+    const coinName = thisCoin.name;
 
-        
-        const coinName = thisCoin.name;
+    const coinABRV = thisCoin.symbol;
 
-        const coinABRV = thisCoin.symbol;
+    const coinPriceToInt = parseFloat(thisCoin.price).toFixed(2);
 
-        const coinPriceToInt = parseFloat(thisCoin.price).toFixed(2);
+    const changeVar = thisCoin.change;
 
-        const changeVar = thisCoin.change;
+    const highestVar = thisCoin.allTimeHigh.timestamp;
 
-        const highestVar = thisCoin.allTimeHigh.timestamp;
+    const highPriceToInt = parseFloat(thisCoin.allTimeHigh.price).toFixed(2);
+    const currentPrice = $("<p>").text(`Current Price: $${coinPriceToInt}`);
 
-        const highPriceToInt = parseFloat(thisCoin.allTimeHigh.price).toFixed(2);
-        const currentPrice = $("<p>").text(`Current Price: $${coinPriceToInt}`);
+    const change = $("<p>").text(`Change: ${changeVar}`);
 
-        const change = $("<p>").text(`Change: ${changeVar}`);
+    const highMarkTime = `Hit on: ${moment(highestVar).format("MM/DD/YYYY")}`;
 
-        const highMarkTime = `Hit on: ${moment(highestVar).format("MM/DD/YYYY")}`
-
-        const highMark = $("<p>").text(`All Time High: $${highPriceToInt} ${highMarkTime}`);
+    const highMark = $("<p>").text(`All Time High: $${highPriceToInt} ${highMarkTime}`);
 
 
 
-        $(coinSearch).text(`${coinName} (${currentDate})`);
-        $(coinDetails).append(currentPrice, change, highMark, saveBtn);
+    $(coinSearch).text(`${coinName} (${currentDate})`);
+    $(coinDetails).append(currentPrice, change, highMark, saveBtn);
 
-        renderMACD(coinABRV);
+    renderMACD(coinABRV);
 
   });
 
 }
 
 $(saveBtn).on("click", function(event) {
-    event.preventDefault();
-    alert("I've been clicked!");
-    // If the user is not signed in, take them to the sign in page
-    window.location.replace("/sign-in");
-    // If the user is signed in, take them to their saved-coins page/portfolio
+  event.preventDefault();
+  alert("I've been clicked!");
+  // If the user is not signed in, take them to the sign in page
+  window.location.replace("/sign-in");
+  // If the user is signed in, take them to their saved-coins page/portfolio
 });
 
 $(".signup").on("click", async function(event) {
-    event.preventDefault();
-     let userInput = $('.user-input').val().trim();
-    const response = await fetch("/api/user", {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        redirect: 'follow', 
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify({username: `${userInput}`}) 
-    });
-    console.log(response)
-})
+  event.preventDefault();
+  let userInput = $(".user-input").val().trim();
+  const response = await fetch("/api/user", {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify({username: `${userInput}`})
+  });
+  console.log(response);
+});
 
 
+$(".sign-in").on("click", async function() {
+  // search through the database and compare what the user entered to the usernames in the database
+  // Once we find their username, pull all of the saved coins for that User
+  // Display all saved coins on /coins route page
 
+  const response = await fetch("/api/user/:id", {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify({Coin: name})
+  });
+
+});
 
 
 
 function renderMACD(coin){
-    console.log("abrev: ", coin);
+  console.log("abrev: ", coin);
 
-    const settings3 = {
-        "async": true,
-        "crossDomain": true,
-        "url": `/api/coin/${coin}`,
-        "method": "GET",
-    };
-    $.ajax(settings3).done(function (response) {
-
-        console.log("MACD:", response);
-        
-    });
-
+  const settings3 = {
+    "async": true,
+    "crossDomain": true,
+    "url": `/api/coin/${coin}`,
+    "method": "GET",
+  };
+  $.ajax(settings3).done(function (response) {
+    console.log("MACD:", response);
+  });
 }
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,40 @@
 // const coinABRV = require("./crypto-investment-tracker/public/assets/js/crypto.js");
-const axios = require('axios');
-const db = require('../models');
+const axios = require("axios");
+const db = require("../models");
 
 module.exports = (app) => {
 
   // Api route to render index.handlebars (dashboard page)
-    app.get('/', (req, res) => res.render('index'));
-  
+  app.get("/", (req, res) => res.render("index"));
+
 
   // Api route to add user
-    app.post('/api/user', (req, res) => {
-      console.log('User Data:', req.body);
-      db.User.create({
-       username: req.body.username
-      }).then((results) => res.json(results));
-    });
-  
+  app.post("/api/user", (req, res) => {
+    console.log("User Data:", req.body);
+    db.User.create({
+      username: req.body.username
+    }).then((results) => res.json(results));
+  });
 
-    
-  
+  // Api route to get user by id and return their coins
+  app.get("/api/user/:id", (req, res) => {
+    db.User.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [db.Coin],
+    }).then((dbCoin) => res.json(dbCoin));
+  });
+
+
 
 
   // MACD call from taapi
-  app.get('/api/coin/:coin', (req, res) => {
+  app.get("/api/coin/:coin", (req, res) => {
     // Require axios: npm i axios
-    
 
-    axios.get('https://api.taapi.io/rsi', {
+
+    axios.get("https://api.taapi.io/rsi", {
       params: {
         secret: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhheWxleXdhaGxyb29zMTVAZ21haWwuY29tIiwiaWF0IjoxNjE0MjA5MzM2LCJleHAiOjc5MjE0MDkzMzZ9.50_xPRl8-tZzpRANXRWBf9VAb2GV31wWyOdIHyZJtD4",
         exchange: "binance",
@@ -34,14 +42,14 @@ module.exports = (app) => {
         interval: "1h",
       }
     })
-    .then(function (response) {
-      console.log("axios", response.data);
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.log(error.response.data);
-    });
-  
+      .then(function (response) {
+        console.log("axios", response.data);
+        res.json(response.data);
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+
   });
 
 };
