a6c0f5745445b87168405e4594d13b52c7b4cc8d
haywah27 <hayleywahlroos15@gmail.com>
2021-03-05 20:50:58 -0800

    css updates to buttons and titles

--- public/assets/js/crypto.js
+++ public/assets/js/crypto.js
@@ -257,7 +257,7 @@ const buttonDump = $(".buttonDump");
 // on search button click, append search item to button list
 function setCoinButton(searchBarCoin){
 
-  const newButton = $("<button class='py-2 px-4 h-10 w-80 border-l-4 border-r-4 border-green-500 bg-black rounded-full text-white'>").text(searchBarCoin);
+  const newButton = $("<button class='py-2 px-4 h-10 w-80 border-l-4 border-r-4 border-green-500 bg-black rounded-full text-white hover:bg-green-500'>").text(searchBarCoin);
   buttonList.push(searchBarCoin);
 
   // retrieving local storage array information
@@ -278,7 +278,7 @@ function displayLocalButton(){
   const coinHistory = JSON.parse(window.localStorage.getItem("coin-name")) || [];
 
   for (let i = 0; i < coinHistory.length; i++){
-    const newButton = $("<button class='py-2 px-4 h-10 w-80 border-l-4 border-r-4 border-green-500 bg-black rounded-full text-white space-wide-1'>").text(coinHistory[i]);
+    const newButton = $("<button class='py-2 px-4 h-10 w-80 border-l-4 border-r-4 border-green-500 bg-black rounded-full text-white space-wide-1 hover:bg-green-500'>").text(coinHistory[i]);
     $(buttonDump).append(newButton);
 
     $(newButton).click(function() {
